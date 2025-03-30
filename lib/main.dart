d91da3e2fa08bc845779fd8eceb3bbfe74607aa0
rafael-escalante <rafaelescalante0607@gmail.com>
2025-03-30 02:33:27 -0600

    chat screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Yes No App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+        title: 'Yes No App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 0).theme(),
+        home: const ChatScreen());
   }
 }
---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/Widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/Widgets/her_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage("https://i.imgflip.com/7n3nhk.jpg"),
+          ),
+        ),
+        title: Text("Mi amor ❤️"),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(itemBuilder: (context, index) {
+              return (index % 2 == 0)
+                  ? const HerMessageBubble()
+                  : const MyMessageBubble();
+            })),
+            Text("Mundo")
+          ],
+        ),
+      ),
+    );
+  }
+}
